9b175bc8e4ba1d0922123f106e5969a4df20ef2f
Pinkipiel <piterwu666@gmail.com>
2015-01-20 21:34:21 +0100

    Autoryzacja, Customowe handlery błędu 404
    + CIasteczka, wylogowanie etc. itp. itd., R.I.P

--- Tennis Organizer4Web/TennisOrganizer.MVC/Controllers/MainController.cs
+++ Tennis Organizer4Web/TennisOrganizer.MVC/Controllers/MainController.cs
@@ -3,25 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 using TennisOrganizer.MVC.Models;
 
 namespace TennisOrganizer.MVC.Controllers
 {
+	[Authorize]
     public class MainController : Controller
     {
         //
         // GET: /Main/
 		TennisOrganizerContext ctx = new TennisOrganizerContext();
-        public ActionResult MainTest()
+		[Authorize]
+		public ActionResult MainTest()
         {
             return View();
         }
-
+		[Authorize]
 		public ActionResult Ranking()
 		{
 			var stats = Player.GetPlayersStats();
 			return View(stats);
 		}
-
+		[Authorize]
+		public ActionResult Challenge()
+		{
+			//zmienić
+			return View();
+		}
+		[Authorize]
+		public ActionResult Training()
+		{
+			return View();
+		}
+		public ActionResult LogOff()
+		{
+			FormsAuthentication.SignOut();
+			return RedirectToAction("Index", "Home");
+		}
     }
 }
