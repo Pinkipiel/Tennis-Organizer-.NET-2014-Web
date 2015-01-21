df6dad381ffcd58b7d1b14e7096688e631113f54
casper01 <kacper133@gmail.com>
2015-01-21 21:59:27 +0100

    Challenge

--- Tennis Organizer4Web/TennisOrganizer.MVC/ViewModels/ChallengeCriteria.cs
+++ Tennis Organizer4Web/TennisOrganizer.MVC/ViewModels/ChallengeCriteria.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using TennisOrganizer.MVC.Models;
@@ -8,12 +10,21 @@ namespace TennisOrganizer.MVC.ViewModels
 {
 	public class ChallengeCriteria
 	{
+		[Required(ErrorMessage="Wprowadź datę spotkania")]
+		[DisplayName("Data (dd.mm.rrrr)")]
 		public DateTime Date { get; set; }
+		[Required(ErrorMessage = "Wprowadź godzinę spotkania")]
+		[DisplayName("Godzina")]
 		public String Hour { get; set; }
+		[DisplayName("Wiek od")]
 		public int AgeFrom { get; set; }
+		[DisplayName("do")]
 		public int AgeTo { get; set; }
+		[DisplayName("Miasto")]
 		public String City { get; set; }
+		[DisplayName("Poziom od")]
 		public float LevelFrom { get; set; }
+		[DisplayName("do")]
 		public float LevelTo { get; set; }
 		public List<Player> SuitableOpponents { get; set; }
 	}
