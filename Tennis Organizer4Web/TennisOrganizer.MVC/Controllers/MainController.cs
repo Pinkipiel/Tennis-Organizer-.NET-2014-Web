fe9856c389c9a619d8c6479067cf998e5dc293e6
Pinkipiel <piterwu666@gmail.com>
2015-01-22 03:38:29 +0100

    Wrzucanie obrazków

--- Tennis Organizer4Web/TennisOrganizer.MVC/Controllers/MainController.cs
+++ Tennis Organizer4Web/TennisOrganizer.MVC/Controllers/MainController.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -34,6 +37,7 @@ namespace TennisOrganizer.MVC.Controllers
 					LoggedInPlayerId = LoggedInPlayer.AccountId;
 					Session.Add("LoggedInPlayerId", (int)LoggedInPlayerId);
 					Session.Add("LoggedInPlayer", (string)User.Identity.Name);
+					Session.Add("Player", (string)LoggedInPlayer.ImagePath);
 				}
 			}
             return View();
@@ -159,13 +163,70 @@ namespace TennisOrganizer.MVC.Controllers
 
 		[HttpPost]
 		[Authorize]
-		public ActionResult ProfileEdition(Player model)
+		public ActionResult ProfileEdition(Player model, HttpPostedFileBase file)
 		{
+			string fileName, fileExt;
+			int width = 140;
+			int height = 140;
 			if(ModelState.IsValid)
 			{
+				if (file != null && file.ContentLength > 0)
+				{
+					char DirSeparator = System.IO.Path.DirectorySeparatorChar;
+					string FilesPath = HttpContext.Server.MapPath("~\\Content" + DirSeparator + "Uploads" + DirSeparator);
+					
+					fileName = (DateTime.Now.Year.ToString() + DateTime.Now.Month.ToString() + DateTime.Now.Day.ToString()
+						+ DateTime.Now.Hour.ToString() + DateTime.Now.Minute.ToString() + DateTime.Now.Second.ToString()) + "_" + (string)Session["LoggedInPlayer"];
+
+					fileExt = Path.GetExtension(file.FileName);
+					if(!fileExt.Contains("jpg") && !fileExt.Contains("png") && !fileExt.Contains("bmp"))
+					{
+						ViewData.Add("WrongFileExtension", (bool)true);
+						return View(model);
+					}
+					if (!Directory.Exists(FilesPath))
+					{
+						Directory.CreateDirectory(FilesPath);
+					}
+					string path = FilesPath + DirSeparator + fileName + fileExt; 
+					file.SaveAs(Path.GetFullPath(path));
+					model.ImagePath = fileName + fileExt;
+
+					//zapisz thumbnail:
+
+					string thumbnailDirectory = String.Format(@"{0}{1}{2}", FilesPath, DirSeparator, "Thumbnails");
+
+					if (!Directory.Exists(thumbnailDirectory))
+					{
+						Directory.CreateDirectory(thumbnailDirectory);
+					}
+
+					string imagePath = String.Format(@"{0}{1}{2}{3}", thumbnailDirectory, DirSeparator, fileName, fileExt);
+					
+					FileStream stream = new FileStream(Path.GetFullPath(imagePath), FileMode.OpenOrCreate);
+
+					Image OrigImage = Image.FromStream(file.InputStream);
+					
+					Bitmap TempBitmap = new Bitmap(width,height);
+
+					Graphics NewImage = Graphics.FromImage(TempBitmap);
+					NewImage.CompositingQuality = CompositingQuality.HighQuality;
+					NewImage.SmoothingMode = SmoothingMode.HighQuality;
+					NewImage.InterpolationMode = InterpolationMode.HighQualityBicubic;
+
+					Rectangle imageRectangle = new Rectangle(0, 0, width, height);
+					NewImage.DrawImage(OrigImage, imageRectangle);
+
+					TempBitmap.Save(stream, OrigImage.RawFormat);
+
+					NewImage.Dispose();
+					TempBitmap.Dispose();
+					OrigImage.Dispose();
+					stream.Close();
+					stream.Dispose();
+				}
 				model.AccountId = (int)Session["LoggedInPlayerId"];
 				model.UpdatePlayer();
-
 			}
 			return View(model);
 		}
